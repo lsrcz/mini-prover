e67f876933486a67263735d8c19dcb5ad2bfe6a9
Sirui Lu <lsrcz@pku.edu.cn>
2018-05-01 01:42:47 +0800

    add lexer

---
+++ src/MiniProver/Parser/Lexer.hs
@@ -0,0 +1,76 @@
+module MiniProver.Parser.Lexer (
+    symbol
+  , parens
+  , colon
+  , coloneq
+  , arrow
+  , larrow
+  , darrow
+  , dot
+  , underscore
+  , rword
+  , ident
+  ) where
+
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type Parser = Parsec Void String
+
+sc :: Parser ()
+sc = L.space space1 lineCmnt blockCmnt
+  where
+    lineCmnt  = L.skipLineComment "--"
+    blockCmnt = L.skipBlockComment "{-" "-}"
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: String -> Parser String
+symbol = L.symbol sc
+
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+colon :: Parser String
+colon = symbol ":"
+
+coloneq :: Parser String
+coloneq = symbol ":="
+
+arrow :: Parser String
+arrow = symbol "->"
+
+larrow :: Parser String
+larrow = symbol "<-"
+
+darrow :: Parser String
+darrow = symbol "=>"
+
+dot :: Parser String
+dot = symbol "."
+
+underscore :: Parser String
+underscore = symbol "_"
+
+rword :: String -> Parser ()
+rword w = (lexeme . try) (string w *> notFollowedBy alphaNumChar)
+
+rws :: [String]
+rws = [
+    "forall", "fun", "fix", "let", "in", "match", "with", "end", "Prop"
+  , "Set", "Type", "Axiom", "Definition", "Inductive", "Fixpoint", "Theorem"
+  , "Proof", "Qed", "exact", "apply", "intro", "intros", "destruct", "induction"
+  , "rewrite", "simpl", "reflexivity", "symmetry", "Print", "Check", "Undo"
+  , "Restart", "Admitted", "Abort"
+  ]
+
+ident :: Parser String
+ident = (lexeme . try) (p >>= check)
+  where
+    p       = (:) <$> letterChar <*> many alphaNumChar
+    check x = if x `elem` rws
+                then fail $ "keyword " ++ show x ++ " cannot be an identifier"
+                else return x
---
+++ test/MiniProver/Parser/LexerSpec.hs
@@ -0,0 +1,34 @@
+module MiniProver.Parser.LexerSpec (main, spec) where
+
+import Test.Hspec
+import Test.Hspec.Megaparsec
+import MiniProver.Parser.Lexer
+import Text.Megaparsec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = 
+  describe "Symbolic" $ do
+    it "colon" $
+      parse colon "" ":" `shouldParse` ":"
+    it "coloneq" $
+      parse coloneq "" ":=" `shouldParse` ":="
+    it "arrow" $
+      parse arrow "" "->" `shouldParse` "->"
+    it "larrow" $
+      parse larrow "" "<-" `shouldParse` "<-"
+    it "darrow" $
+      parse darrow "" "=>" `shouldParse` "=>"
+    it "dot" $
+      parse dot "" "." `shouldParse` "."
+    it "underscore" $
+      parse underscore "" "_" `shouldParse` "_"
+    it "rws" $
+      parse (rword "forall") "" "forall" `shouldParse` ()
+    it "ident non-rws" $
+      parse ident "" "abcd" `shouldParse` "abcd"
+    it "ident rws" $
+      parse ident "" `shouldFailOn` "Abort"
+      
