05647e0fc1f72c57f3cd88f91cbc08d61cbda908
Shlw <shlw_kevin@hotmail.com>
2018-05-15 13:11:00 +0800

    repair build.hs

--- src/MiniProver/Core/Build.hs
+++ src/MiniProver/Core/Build.hs
@@ -28,10 +28,19 @@ buildTerm :: Term -> BuiltTerm
 buildTerm (TmVar name) ctx = 
   case nameToIndex ctx name of
     Right i -> TmRel name i
-    Left IsConstructor -> TmConstrRef name
-    Left IsTypeConstructor -> TmIndTypeRef name
+    Left IsConstructor -> 
+      fromRight (error "This should not happen") $ getConstrTerm ctx name
+    Left IsTypeConstructor -> 
+      fromRight (error "This should not happen") $ getIndTypeTerm ctx name
     _ -> error "This should not happen"
-buildTerm (TmAppl lst) ctx = TmAppl $ map (`buildTerm` ctx) lst
+buildTerm (TmAppl lst) ctx =
+  case map (`buildTerm` ctx) lst of
+    [] -> error "This should not happen"
+    [h] -> h
+    ls@(x:xs) ->
+      case x of
+        TmAppl ls' -> TmAppl $ ls' ++ xs
+        _ -> TmAppl ls
 buildTerm (TmProd name ty tm) ctx = 
   TmProd name (buildTerm ty ctx) (buildTerm tm (addName ctx name))
 buildTerm (TmLambda name ty tm) ctx = 
@@ -48,7 +57,7 @@ buildTerm (TmIndType name tmlst) ctx =
 buildTerm (TmConstr name tmlst) ctx =
   TmConstr name $
     map (`buildTerm` ctx) tmlst
-buildTerm (TmSort s) _ = TmSort s
+buildTerm TmType _ = TmType
 buildTerm (TmMatch tm namelst rty equlst) ctx =
   TmMatch (buildTerm tm ctx) namelst (buildTerm rty (foldl addName ctx (tail namelst))) $
     map (`buildEquation` ctx) equlst
