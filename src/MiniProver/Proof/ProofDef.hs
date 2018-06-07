4e2691e7a38a5c901ad4f04d8e5bef1458ba1af1
Sirui Lu <lsrcz@pku.edu.cn>
2018-06-07 12:47:33 +0800

    Show instance for Result

--- src/MiniProver/Proof/ProofDef.hs
+++ src/MiniProver/Proof/ProofDef.hs
@@ -30,9 +30,12 @@ data ProofInput =
   | PTac Tactic
   deriving (Show, Eq)
 
-newtype TacticError = TacticError String
+newtype TacticError = TacticError String deriving (Show, Eq)
 
 data Goal = Goal Int Context Term deriving (Show, Eq)
 
 type ResultFunc = [Term] -> Term
 data Result = Result [Goal] ResultFunc
+
+instance Show Result where
+  show (Result goallst f) = "Goals\n" ++ unlines (map show goallst) ++ "\nfunc\n" ++ show (f [TmVar ("Goal" ++ show i) | i <- [1..]])
