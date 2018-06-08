944ef5aa7f6befdbf5cb1cd043f3df3eaa8f4b6d
Sirui Lu <lsrcz@pku.edu.cn>
2018-06-08 21:40:52 +0800

    add supporting functions

--- src/MiniProver/Proof/ProofDef.hs
+++ src/MiniProver/Proof/ProofDef.hs
@@ -37,5 +37,11 @@ data Goal = Goal Int Context Term deriving (Show, Eq)
 type ResultFunc = [Term] -> Term
 data Result = Result [Goal] ResultFunc
 
+getGoalList :: Result -> [Goal]
+getGoalList (Result goallst _) = goallst
+
+getResultFunc :: Result -> ResultFunc
+getResultFunc (Result _ rf) = rf
+
 instance Show Result where
   show (Result goallst f) = "Goals\n" ++ unlines (map show goallst) ++ "\nfunc\n" ++ show (f [TmVar ("Goal" ++ show i) | i <- [1..]])
