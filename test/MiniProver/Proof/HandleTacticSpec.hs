1f6771fd1c09a479f3724a8f476ee39fa1cb9a42
Shlw <shlw_kevin@hotmail.com>
2018-06-09 00:31:12 +0800

    fix bugs in test

--- test/MiniProver/Proof/HandleTacticSpec.hs
+++ test/MiniProver/Proof/HandleTacticSpec.hs
@@ -22,12 +22,12 @@ spec = do
           TmType
         ( TmProd "_"
           ( TmRel "T" 0 )
-          ( TmProd "m0"
+          ( TmProd "m"
             ( TmRel "T" 1 )
             ( TmIndType "eq"
               [ TmRel "T" 2
-              , TmRel "m0" 0
-              , TmRel "m0" 0 ])))
+              , TmRel "m" 0
+              , TmRel "m" 0 ])))
       allinoneAns = fromRight undefined $ handleTactic goal
         ( Exact
           ( TmLambda "T"
@@ -66,24 +66,24 @@ spec = do
           TmType
         ( TmProd "_"
           ( TmRel "T" 0 )
-          ( TmProd "m0"
+          ( TmProd "m"
             ( TmRel "T" 1 )
             ( TmIndType "eq"
               [ TmRel "T" 2
-              , TmRel "m0" 0
-              , TmRel "m0" 0 ])))
+              , TmRel "m" 0
+              , TmRel "m" 0 ])))
       nonameAns = fromRight undefined $ handleTactic goal (Intro [])
     it "no name -- goal" $
       getGoalList nonameAns `shouldBe`
       [ Goal 1 (("T",VarBind TmType):realEqContext)
         ( TmProd "_"
           ( TmRel "T" 0 )
-          ( TmProd "m0"
+          ( TmProd "m"
             ( TmRel "T" 1 )
             ( TmIndType "eq"
               [ TmRel "T" 2
-              , TmRel "m0" 0
-              , TmRel "m0" 0 ])))]
+              , TmRel "m" 0
+              , TmRel "m" 0 ])))]
     it "no name -- func" $
       getResultFunc nonameAns [TmVar "Goal1"] `shouldBe`
         TmLambda "T" TmType (TmVar "Goal1")
@@ -92,19 +92,19 @@ spec = do
       getGoalList onenameAns `shouldBe`
       [ Goal 1 (("T1",VarBind TmType):realEqContext)
         ( TmProd "_"
-          ( TmRel "T" 0 )
-          ( TmProd "m0"
+          ( TmRel "T1" 0 )
+          ( TmProd "m"
             ( TmRel "T1" 1 )
             ( TmIndType "eq"
               [ TmRel "T1" 2
-              , TmRel "m0" 0
-              , TmRel "m0" 0 ])))]
+              , TmRel "m" 0
+              , TmRel "m" 0 ])))]
     it "one name -- func" $
       getResultFunc onenameAns [TmVar "Goal1"] `shouldBe`
         TmLambda "T1" TmType (TmVar "Goal1")
     let twonamesAns = fromRight undefined $ handleTactic goal (Intro ["T1","m"])
     it "two names -- goal" $
-      getGoalList onenameAns `shouldBe`
+      getGoalList twonamesAns `shouldBe`
       [ Goal 2 (("m",VarBind (TmRel "T1" 0)):("T1",VarBind TmType):realEqContext)
         ( TmProd "m0"
           ( TmRel "T1" 1 )
@@ -113,7 +113,7 @@ spec = do
             , TmRel "m0" 0
             , TmRel "m0" 0 ]))]
     it "two names -- func" $
-      getResultFunc onenameAns [TmVar "Goal1"] `shouldBe`
+      getResultFunc twonamesAns [TmVar "Goal1"] `shouldBe`
         TmLambda "T1" TmType (TmLambda "m" (TmRel "T1" 0) (TmVar "Goal1"))
     it "four names -- should fail" $
       handleTactic goal (Intro ["a","b","c","d"]) `shouldSatisfy`
@@ -127,23 +127,23 @@ spec = do
           TmType
         ( TmProd "_"
           ( TmRel "T" 0 )
-          ( TmProd "m0"
+          ( TmProd "m"
             ( TmRel "T" 1 )
             ( TmIndType "eq"
               [ TmRel "T" 2
-              , TmRel "m0" 0
-              , TmRel "m0" 0 ])))
+              , TmRel "m" 0
+              , TmRel "m" 0 ])))
       allinoneAns = fromRight undefined $ handleTactic goal Intros
     it "all in one -- goal" $
       getGoalList allinoneAns `shouldBe`
-      [ Goal 3 (("m0",VarBind (TmRel "T1" 1)):("t",VarBind (TmRel "T1" 0)):("T1",VarBind TmType):realEqContext)
+      [ Goal 3 (("m",VarBind (TmRel "T" 1)):("t",VarBind (TmRel "T" 0)):("T",VarBind TmType):realEqContext)
         ( TmIndType "eq"
-          [ TmRel "T1" 2
-          , TmRel "m0" 0
-          , TmRel "m0" 0 ])]
+          [ TmRel "T" 2
+          , TmRel "m" 0
+          , TmRel "m" 0 ])]
     it "all in one -- func" $
       getResultFunc allinoneAns [TmVar "Goal1"] `shouldBe`
-        TmLambda "T1" TmType (TmLambda "t" (TmRel "T1" 0) (TmLambda "m0" (TmRel "T1" 1) (TmVar "Goal1")))
+        TmLambda "T" TmType (TmLambda "t" (TmRel "T" 0) (TmLambda "m" (TmRel "T" 1) (TmVar "Goal1")))
   describe "destruct" $ do
     let
       goal = Goal 0 
