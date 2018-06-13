1cd2bd0675887b5bed233f30468e28bd90c9505d
Shlw <shlw_kevin@hotmail.com>
2018-06-13 20:11:02 +0800

    shut off verbose mode

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,8 @@ main :: IO ()
 main = do
   handle <- openFile "./libs/Init/Prelude.1.v" ReadMode
   p <- processFile handle 0 []
-  ctx <- topLoop 3 p --ilistContext
+  --ctx <- topLoop 3 p --ilistContext
+  ctx <- topLoop 0 p --ilistContext
   print ctx
 
 {-
